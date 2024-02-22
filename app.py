5242fb289ff9d7e2ef3d91dceadea5981fb9ed63
Grace Gallivan <gracee.gallivan@gmail.com>
2024-02-21 18:03:41 -0800

    added results route

--- app.py
+++ app.py
@@ -12,15 +12,25 @@ debug = DebugToolbarExtension(app)
 # ROUTES: homepage, results
 
 # HOMEPAGE: dynamic based on story
-@app.get('/homepage')
+@app.get('/')
 def get_story_form():
-# generates question form using quersitons template
+    # generates question form using questions template
+
+    prompts = silly_story.prompts
 
     return render_template(
         'questions.html',
-        prompts = silly_story.prompts)
+        prompts=prompts)
 
 
 # RESULTS: Whole story put together here
-# @app.get('/results')
-# def get_results():
+@app.get('/results')
+def get_results():
+    # generates madlib story using story form inputs
+
+    text = silly_story.get_result_text(request.args)
+
+    return render_template(
+        'results.html',
+        generated_story=text
+        )
